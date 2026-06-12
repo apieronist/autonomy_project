1cfb9063a48f235c2d0a67ede37001fb45ef0653
apieroni <apieronist@gmail.com>
2026-06-11 23:29:54 -0300

    Forzado de extensiones html en docs

--- freeze.py
+++ freeze.py
@@ -2,10 +2,16 @@ from flask_frozen import Freezer
 from app import app
 
 app.config['FREEZER_DESTINATION'] = 'docs'
-# Esta línea hace que los enlaces funcionen de forma relativa en cualquier servidor
-app.config['FREEZER_RELATIVE_URLS'] = True 
+app.config['FREEZER_RELATIVE_URLS'] = True
 
 freezer = Freezer(app)
+
+# Fuerza a Flask a mapear las rutas con extensión .html
+@freezer.register_generator
+def route_generator():
+    yield 'index', {}
+    yield 'medications', {}
+    yield 'contacts', {}
 
 if __name__ == '__main__':
     freezer.freeze()
